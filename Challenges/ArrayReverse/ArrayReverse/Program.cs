fcdc3e245f2e5c7919fa8b13b6575d5ff4d3fc74
Jesse Atay <jesse.atay@gmail.com>
2018-06-05 02:12:35 -0700

    Array reverse method works, print to page with results complete

--- Challenges/ArrayReverse/ArrayReverse/Program.cs
+++ Challenges/ArrayReverse/ArrayReverse/Program.cs
@@ -8,31 +8,36 @@ namespace ArrayReverse
         {
 
             //test array
-            int[] TestArray = { 1, 2, 3, 4, 5, 6 };
-            PrintArray(TestArray);
+            Console.WriteLine("Test Array Values");
+            int[] TestArray = {1, 2, 3, 4, 5, 6, 77, -88, 9, 10000};
 
-    
+            //printing the values of the test array for comparison
+            PrintArray(TestArray);
+            Console.WriteLine("");
+
+            //performing reverse array method
             int[] result = ReverseArray(TestArray);
-            Console.WriteLine("Result of reverse Method");
+
+            //printing values of reversed array for comparison
+            Console.WriteLine("Reversed Array Values");
             PrintArray(result);
 
+            //read line to pause before exit
+            Console.ReadLine();
         }
 
         //method to reverse array
         static int[] ReverseArray(int[] InputArray)
         {
             int[] newArray = new int[InputArray.Length];
+            int HoldValue = 0;
 
             for (int i = InputArray.Length-1; i >= 0; i--)
             {
-                int newArrayCounter = 0;
-                Console.Write(InputArray[i]);
-                newArray[newArrayCounter] = InputArray[i];
-                newArrayCounter++;
-              
-                
+                newArray[HoldValue] = InputArray[i];
+                HoldValue++;
             }
-          
+           
             return newArray;
         }
 
@@ -41,10 +46,9 @@ namespace ArrayReverse
         {
             foreach(var item in ArrayToPrint)
             {
-                Console.Write(item.ToString());
+                item.ToString();
             }
+            Console.Write(string.Join(",", ArrayToPrint));
         }
-
-        
     }
 }
