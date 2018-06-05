df8c01099db31f3e11d254060cc0c211f3fea307
Jesse Atay <jesse.atay@gmail.com>
2018-06-05 12:54:39 -0700

    Search method works and returns appropriate values, ready for testing

--- Challenges/BinarySearch/BinarySearch/BinarySearch/Program.cs
+++ Challenges/BinarySearch/BinarySearch/BinarySearch/Program.cs
@@ -6,7 +6,32 @@ namespace BinarySearch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //test array and key, adjust as necessary
+            int[] TestArray = { 4, 8, 15, 16, 23, 42 };
+            int TestKey = 23;
+
+            //binary search method executed on test array and key
+            BinarySearch(TestArray, TestKey);
+
+            Console.WriteLine("Press any key to exit program.");
+            Console.ReadLine();
+        }
+
+
+        //method to search an array with a given key. Result printed to console as either array index if found, or -1 if not found
+        static int BinarySearch(int[] InputArray, int Key)
+        {
+            for (int i = 0; i < InputArray.Length; i++)
+            {
+                if (InputArray[i] == Key)
+                {
+                    int FoundIt = i;
+                    Console.WriteLine(FoundIt);
+                    return FoundIt;
+                }
+            }
+            Console.WriteLine(-1);
+            return -1; 
         }
     }
 }
