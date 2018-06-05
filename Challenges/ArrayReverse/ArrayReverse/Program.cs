6acf203837e177d2d722103fa8ad644d5e077624
Jesse Atay <jesse.atay@gmail.com>
2018-06-05 01:19:19 -0700

    Prototype with test array, half time build

--- Challenges/ArrayReverse/ArrayReverse/Program.cs
+++ Challenges/ArrayReverse/ArrayReverse/Program.cs
@@ -6,17 +6,45 @@ namespace ArrayReverse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //test array
+            int[] TestArray = { 1, 2, 3, 4, 5, 6 };
+            PrintArray(TestArray);
+
+    
+            int[] result = ReverseArray(TestArray);
+            Console.WriteLine("Result of reverse Method");
+            PrintArray(result);
 
         }
 
-        static int[] ReverseArray(input)
+        //method to reverse array
+        static int[] ReverseArray(int[] InputArray)
         {
-            int[] newArray = new int[input.length];
-            return newArray
+            int[] newArray = new int[InputArray.Length];
+
+            for (int i = InputArray.Length-1; i >= 0; i--)
+            {
+                int newArrayCounter = 0;
+                Console.Write(InputArray[i]);
+                newArray[newArrayCounter] = InputArray[i];
+                newArrayCounter++;
+              
+                
+            }
+          
+            return newArray;
         }
 
-        //test array
-        int[] TestArray = {1, 2, 3, 4, 5, 6};
+        //method to print array to console
+        static void PrintArray(int[] ArrayToPrint)
+        {
+            foreach(var item in ArrayToPrint)
+            {
+                Console.Write(item.ToString());
+            }
+        }
+
+        
     }
 }
