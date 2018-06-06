5263b458d9c7b343a24a11a728218e8ff0ebb33d
Jesse Atay <jesse.atay@gmail.com>
2018-06-06 10:32:22 -0700

    updates, algorithm functional with unit tests

--- Challenges/BinarySearch/BinarySearch/BinarySearch/Program.cs
+++ Challenges/BinarySearch/BinarySearch/BinarySearch/Program.cs
@@ -2,24 +2,23 @@
 
 namespace BinarySearch
 {
-    class Program
+   public class Program
     {
-        static void Main(string[] args)
+       public static void Main(string[] args)
         {
             //test array and key, adjust as necessary
-            int[] TestArray = { 4, 8, 15, 16, 23, 42 };
+            int[] TestArray = { 4, 8, 15, 16, 23, 30, 42 };
             int TestKey = 23;
 
             //binary search method executed on test array and key
             BinarySearch(TestArray, TestKey);
 
-            Console.WriteLine("Press any key to exit program.");
-            Console.ReadLine();
+            Console.WriteLine("Press enter to exit program.");
+            while(Console.ReadKey().Key != ConsoleKey.Enter);
         }
 
-
         //method to search an array with a given key. Result printed to console as either array index if found, or -1 if not found
-        static int BinarySearch(int[] InputArray, int Key)
+       public static int BinarySearch(int[] InputArray, int Key)
         {
             for (int i = 0; i < InputArray.Length; i++)
             {
