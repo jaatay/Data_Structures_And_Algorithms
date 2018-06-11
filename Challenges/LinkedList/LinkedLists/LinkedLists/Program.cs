2585e77fd75e3d4a4767e9c32c7803bd164a255f
Jesse Atay <jesse.atay@gmail.com>
2018-06-11 16:41:58 -0700

    additional methods and initial tests

--- Challenges/LinkedList/LinkedLists/LinkedLists/Program.cs
+++ Challenges/LinkedList/LinkedLists/LinkedLists/Program.cs
@@ -7,6 +7,21 @@ namespace LinkedLists
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+			FirstLL();
+			
         }
+
+		static void FirstLL()
+		{
+			LinkedList LL = new LinkedList(new Node(10));
+
+			LL.Add(new Node(15));
+			LL.Add(new Node(20));
+			LL.Add(new Node(25));
+			LL.Add(new Node(30));
+
+			LL.Print();
+			LL.AddValues();
+		}
     }
 }
