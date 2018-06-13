fb7ea82c498467687b769dfe5a4abec21c8ccf2c
Jesse Atay <jesse.atay@gmail.com>
2018-06-13 11:23:04 -0700

    kth from end challenge

---
+++ Challenges/LL_Kth_From_End/LL_Kth_From_End/LL_Kth_From_End/Classes/LinkList.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace LL_Kth_From_End { 
+public class LinkList	
+{
+	/// <summary>
+	/// gets and sets the value of head
+	/// </summary>
+	public Node Head { get; set; }
+
+	/// <summary>
+	/// gets and sets the value of current
+	/// </summary>
+	public Node Current { get; set; }
+
+	/// <summary>
+	/// gets and sets value of runner node
+	/// </summary>
+	public Node Runner { get; set; }
+
+	/// <summary>
+	/// method to identify a node
+	/// </summary>
+	/// <param name="node">input paramter of node to set as head and current</param>
+	public LinkList(Node node)
+	{
+		Head = node;
+		Current = node;
+	}
+	/// <summary>
+	/// method to add new node in front of list, sets as head node
+	/// </summary>
+	/// <param name="node"></param>
+	public void Add(Node node)
+	{
+		node.Next = Head;
+		Head = node;
+		Current = Head;
+	}
+
+	
+	/// <summary>
+	/// method to print linked list to console
+	/// </summary>
+	public void Print()
+	{
+		Current = Head;
+
+		while (Current.Next != null)
+		{
+			Console.Write($"{Current.Value} ==>");
+			Current = Current.Next;
+		}
+
+		Console.Write($"{Current.Value} ==> NULL");
+		Console.WriteLine("");
+	}
+
+		/// <summary>
+		/// method to find kth element in list
+		/// </summary>
+		/// <param name="k">integer of value to be found from the end of the list</param>
+		/// <returns>returns value of node found, or throws index out of range exception if k is outside the list</returns>
+		public Node KthElement(int k)
+		{
+			try
+			{
+				if(k < 0)
+				{
+					throw new IndexOutOfRangeException();
+				}
+				Current = Head;
+				Runner = Head;
+				int counter = 0;
+
+				while (Runner.Next != null)
+				{
+					counter++;
+					Runner = Runner.Next;
+
+					if (counter > k)
+					{
+						Current = Current.Next;
+					}
+
+				}
+
+				Console.WriteLine($"Found node {Current.Value} ");
+				return Current;
+			}
+			catch (IndexOutOfRangeException)
+			{
+				Console.WriteLine("Value not found");
+				
+			}
+
+			return null;
+		}
+		
+
+	
+
+}
+
+}
---
+++ Challenges/LL_Kth_From_End/LL_Kth_From_End/LL_Kth_From_End/Classes/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace LL_Kth_From_End
+{
+	public class Node
+	{
+		/// <summary>
+		/// gets and sets value property of a node
+		/// </summary>
+		public int Value { get; set; }
+
+		/// <summary>
+		/// gets and sets the next property of a node
+		/// </summary>
+		public Node Next { get; set; }
+
+		/// <summary>
+		/// sets the valute of a node
+		/// </summary>
+		/// <param name="value">integer to use as node value</param>
+		public Node(int value)
+		{
+			Value = value;
+		}
+	}
+}
