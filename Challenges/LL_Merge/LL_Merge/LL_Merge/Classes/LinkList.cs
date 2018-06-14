d1d1e61c173b4cd815bd4491b2c1e209a31bf830
Jesse Atay <jesse.atay@gmail.com>
2018-06-14 10:34:31 -0700

    strange error correction

---
+++ Challenges/LL_Merge/LL_Merge/LL_Merge/Classes/LinkList.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using LL_Merge;
+
+namespace LL_Merge
+{
+	public class LinkList
+	{
+		/// <summary>
+		/// gets and sets the value of head
+		/// </summary>
+		public Node Head { get; set; }
+
+		/// <summary>
+		/// gets and sets the value of current
+		/// </summary>
+		public Node Current { get; set; }
+
+		/// <summary>
+		/// gets and sets value of runner node
+		/// </summary>
+		public Node refNode1 { get; set; }
+
+		public Node refNode2 { get; set; }
+
+		/// <summary>
+		/// method to identify a node
+		/// </summary>
+		/// <param name="node">input paramter of node to set as head and current</param>
+		public LinkList(Node node)
+		{
+			Head = node;
+			Current = node;
+		}
+		/// <summary>
+		/// method to add new node in front of list, sets as head node
+		/// </summary>
+		/// <param name="node"></param>
+		public void Add(Node node)
+		{
+			node.Next = Head;
+			Head = node;
+			Current = Head;
+		}
+
+
+		/// <summary>
+		/// method to print linked list to console
+		/// </summary>
+		public void Print()
+		{
+			Current = Head;
+
+			while (Current.Next != null)
+			{
+				Console.Write($"{Current.Value} ==>");
+				Current = Current.Next;
+			}
+
+			Console.Write($"{Current.Value} ==> NULL");
+			Console.WriteLine("");
+		}
+
+		public Node Merge(LinkList LL1, LinkList LL2)
+		{
+			refNode1 = LL1.Head;
+			refNode2 = LL2.Head;
+			LL1.Current = LL1.Head;
+			LL2.Current = LL2.Head;
+
+			while((LL1.Current.Next != null) && (LL2.Current.Next != null))
+			{
+				refNode1 = LL1.Current.Next;
+				refNode2 = LL2.Current.Next;
+				LL1.Current.Next = LL2.Current;
+				LL2.Current.Next = refNode1;
+				LL2.Current = refNode2;
+				LL1.Current = refNode1;
+			}
+			if((LL2.Current.Next == null) && (LL1.Current.Next != null))
+			{
+				LL2.Current.Next = LL1.Current.Next;
+				LL1.Current.Next = LL2.Current;
+			} else
+			{
+				LL1.Current.Next = LL2.Current;
+			}
+			return LL1.Head;
+		}
+	}
+}
--- challenges/LL_Merge/LL_Merge/LL_Merge/Program.cs
+++ challenges/LL_Merge/LL_Merge/LL_Merge/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace LL_Merge
 {
@@ -7,6 +9,21 @@ namespace LL_Merge
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+			NewLL();
         }
+
+		static void NewLL()
+		{
+			LinkList LL = new LinkList(new Node(1));
+
+			LL.Add(new Node(2));
+			LL.Add(new Node(3));
+			LL.Add(new Node(4));
+			LL.Add(new Node(5));
+
+
+			LL.Print();
+			
+		}
     }
 }
