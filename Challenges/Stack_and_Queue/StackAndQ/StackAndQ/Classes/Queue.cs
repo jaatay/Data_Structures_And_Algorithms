316e957c8cab566c857449b67535413fa7fc2893
Jesse Atay <jesse.atay@gmail.com>
2018-06-17 22:36:04 -0700

    updates: stack works

--- Challenges/Stack_and_Queue/StackAndQ/StackAndQ/Classes/Queue.cs
+++ Challenges/Stack_and_Queue/StackAndQ/StackAndQ/Classes/Queue.cs
@@ -1,8 +1,112 @@
 ﻿using System;
 
-public class Class1
+namespace StackAndQ
 {
-	public Class1()
+	public class Queue
 	{
+		
+			/// <summary>
+			/// gets and sets the value of head
+			/// </summary>
+			public Node Head { get; set; }
+
+			/// <summary>
+			/// gets and sets the value of current
+			/// </summary>
+			public Node Current { get; set; }
+
+			/// <summary>
+			/// method to identify a node
+			/// </summary>
+			/// <param name="node">input paramter of node to set as head and current</param>
+			public Queue(Node node)
+			{
+				Head = node;
+				Current = node;
+			}
+
+			/// <summary>
+			/// method to add new node in front of list, sets as head node, used to create new queue list
+			/// </summary>
+			/// <param name="node"></param>
+			public void Add(Node node)
+			{
+				node.Next = Head;
+				Head = node;
+				Current = Head;
+			}
+
+			/// <summary>
+			/// method to add node to end of list
+			/// </summary>
+			/// <param name="newNode">new node to add</param>
+			public void Enqueue(Node newNode)
+			{
+				Current = Head;
+				while (Current.Next != null)
+				{
+					Current = Current.Next;
+				}
+
+				Current.Next = newNode;
+			}
+
+			/// <summary>
+			/// method to add node to end of list
+			/// </summary>
+			/// <param name="newNode">new node to add</param>
+			public void Dequeue()
+			{
+				Current = Head;
+				Head = Current.Next;
+
+			}
+
+			/// <summary>
+			/// method to add node to end of list
+			/// </summary>
+			/// <param name="newNode">new node to add</param>
+			public Node Peek()
+			{
+
+				try
+				{
+					Current = Head;
+					while (Current.Next != null)
+					{
+						Current = Current.Next;
+					}
+
+					if (Head == null)
+					{
+						throw new IndexOutOfRangeException();
+					}
+					Console.WriteLine($"Found {Head.Value}");
+					return Head;
+				}
+				catch (IndexOutOfRangeException)
+				{
+					Console.WriteLine("Stack is empty.");
+					return null;
+				}
+			}
+
+			/// <summary>
+			/// method to print linked list to console
+			/// </summary>
+			public void PrintStack()
+			{
+				Current = Head;
+
+				while (Current.Next != null)
+				{
+					Console.Write($"{Current.Value} ==>");
+					Current = Current.Next;
+				}
+
+				Console.Write($"{Current.Value} ==> NULL");
+				Console.WriteLine("");
+			}
+
 	}
 }
--- Challenges/Stack_and_Queue/StackAndQ/StackAndQ/Classes/Stack.cs
+++ Challenges/Stack_and_Queue/StackAndQ/StackAndQ/Classes/Stack.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace StackAndQ
 {
-	public class LinkList
+	public class Stack
 	{
 		/// <summary>
 		/// gets and sets the value of head
@@ -20,11 +20,12 @@ namespace StackAndQ
 		/// method to identify a node
 		/// </summary>
 		/// <param name="node">input paramter of node to set as head and current</param>
-		public LinkList(Node node)
+		public Stack(Node node)
 		{
 			Head = node;
 			Current = node;
 		}
+
 		/// <summary>
 		/// method to add new node in front of list, sets as head node
 		/// </summary>
@@ -37,9 +38,64 @@ namespace StackAndQ
 		}
 
 		/// <summary>
+		/// method to add node to end of list
+		/// </summary>
+		/// <param name="newNode">new node to add</param>
+		public void Push(Node newNode)
+		{
+			Current = Head;
+			while (Current.Next != null)
+			{
+				Current = Current.Next;
+			}
+
+			Current.Next = newNode;
+		}
+
+		/// <summary>
+		/// method to add node to end of list
+		/// </summary>
+		/// <param name="newNode">new node to add</param>
+		public void Pop()
+		{
+			Current = Head;
+			Head = Current.Next;
+		
+		}
+
+		/// <summary>
+		/// method to add node to end of list
+		/// </summary>
+		/// <param name="newNode">new node to add</param>
+		public Node Peek()
+		{
+
+			try
+			{
+				Current = Head;
+				while (Current.Next != null)
+				{
+					Current = Current.Next;
+				}
+
+				if (Head == null)
+				{
+					throw new IndexOutOfRangeException();
+				}
+				Console.WriteLine($"Found {Head.Value}");
+				return Head;
+			}
+			catch (IndexOutOfRangeException)
+			{
+				Console.WriteLine("Stack is empty.");
+				return null;
+			}
+		}
+
+		/// <summary>
 		/// method to print linked list to console
 		/// </summary>
-		public void Print()
+		public void PrintStack()
 		{
 			Current = Head;
 
@@ -53,112 +109,6 @@ namespace StackAndQ
 			Console.WriteLine("");
 		}
 
-		/// <summary>
-		/// method to add sum of linked list values(not required part of lab, just did it out of curiosity)
-		/// </summary>
-		public void AddValues()
-		{
-			Current = Head;
-			int valueCount = 0;
-
-			while (Current.Next != null)
-			{
-				Current = Current.Next;
-				valueCount += Current.Value;
-			}
-
-			Console.WriteLine($"The sum is: {valueCount}");
-			Console.WriteLine("");
-
-		}
-
-		/// <summary>
-		/// method to find the location of a node based on value
-		/// </summary>
-		/// <param name="value">value of node to find in list</param>
-		/// <returns>returns value of found node as current, unless node is not found then returns null</returns>
-		public Node Find(int value)
-		{
-			Current = Head;
-
-			while (Current.Next != null)
-			{
-				if (Current.Value == value)
-				{
-					return Current;
-				}
-				Current = Current.Next;
-			}
-			return Current.Value == value ? Current : null;
-		}
-
-		/// <summary>
-		/// method to add node to end of list
-		/// </summary>
-		/// <param name="newNode">new node to add</param>
-		public void AddLast(Node newNode)
-		{
-			Current = Head;
-			while (Current.Next != null)
-			{
-				Current = Current.Next;
-			}
-
-			Current.Next = newNode;
-		}
-
-		/// <summary>
-		/// method to add node after a currently existing node
-		/// </summary>
-		/// <param name="newNode">new node to add</param>
-		/// <param name="existingNode">existing node to add new node after</param>
-		public void AddAfter(Node newNode, Node existingNode)
-		{
-			Current = Head;
-
-			if (Head.Value == existingNode.Value)
-			{
-				Add(newNode);
-				return;
-			}
-
-			while (Current.Next != null)
-			{
-				if (Current.Next.Value == existingNode.Value)
-				{
-					newNode.Next = Current.Next;
-					Current.Next = newNode;
-				}
-				Current = Current.Next;
-			}
-		}
-
-		/// <summary>
-		/// method to add new node before currently existing node
-		/// </summary>
-		/// <param name="newNode">new node to add</param>
-		/// <param name="existingNode">node to add new node before</param>
-		public void AddBefore(Node newNode, Node existingNode)
-		{
-			//reset current to beginning of LL
-			Current = Head;
-
-			if (Head.Value == existingNode.Value)
-			{
-				Add(newNode);
-				return;
-			}
-
-			while (Current.Next != null)
-			{
-				if (Current.Next.Value == existingNode.Value)
-				{
-					newNode.Next = Current.Next;
-					Current.Next = newNode;
-					return;
-				}
-				Current = Current.Next;
-			}
-		}
+		
 	}
 }
