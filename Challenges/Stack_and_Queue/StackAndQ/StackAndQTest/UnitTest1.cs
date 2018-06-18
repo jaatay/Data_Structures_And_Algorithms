1bf1ee1651b1e0c3aecf49ece6729b93bb7b7b4c
Jesse Atay <jesse.atay@gmail.com>
2018-06-17 23:40:58 -0700

    Stack and Queue complete, tests passed

--- Challenges/Stack_and_Queue/StackAndQ/StackAndQTest/UnitTest1.cs
+++ Challenges/Stack_and_Queue/StackAndQ/StackAndQTest/UnitTest1.cs
@@ -1,14 +1,118 @@
 using System;
 using Xunit;
+using StackAndQ;
 
 namespace StackAndQTest
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+		[Fact]
+		public void CanPushStack()
+		{
+			//arrange
+			Stack testStack = new Stack(new Node(1));
+			Node node2 = new Node(2);
+			Node node3 = new Node(3);
+			Node node4 = new Node(4);
 
-        }
-    }
+			//act
+			testStack.Push(node2);
+			testStack.Push(node3);
+
+			//assert
+			Assert.Equal(testStack.Head.Value, node3.Value);
+		}
+
+		[Fact]
+		public void CanPopStack()
+		{
+			//arrange
+			Stack testStack = new Stack(new Node(1));
+			Node node2 = new Node(2);
+			Node node3 = new Node(3);
+			Node node4 = new Node(4);
+
+			//act
+			testStack.Push(node2);
+			testStack.Push(node3);
+			testStack.Pop();
+
+			//assert
+			Assert.Equal(testStack.Head.Value, node2.Value);
+		}
+
+		[Fact]
+		public void CanPeekStack()
+		{
+			//arrange
+			Stack testStack = new Stack(new Node(1));
+			Node node2 = new Node(2);
+			Node node3 = new Node(3);
+			Node node4 = new Node(4);
+
+			//act
+			testStack.Push(node2);
+			testStack.Push(node3);
+
+
+			//assert
+			Assert.Equal(testStack.Head.Value, testStack.Peek().Value);
+		}
+
+		[Fact]
+		public void CanEnqueueQ()
+		{
+			//arrange
+			Queue testQ = new Queue(new Node(1));
+			Node node2 = new Node(2);
+			Node node3 = new Node(3);
+			Node node4 = new Node(4);
+
+			//act
+			testQ.Enqueue(node2);
+			testQ.Enqueue(node3);
+
+			//assert
+			Assert.Equal(testQ.Head.Value, node3.Value);
+		}
+
+		[Fact]
+		public void CanDequeueQ()
+		{
+			//arrange
+			Queue testQ = new Queue(new Node(1));
+			Node node2 = new Node(2);
+			Node node3 = new Node(3);
+			Node node4 = new Node(4);
+
+			//act
+			testQ.Enqueue(node2);
+			testQ.Enqueue(node3);
+			testQ.Enqueue(node4);
+			testQ.Dequeue();
+
+			//assert
+			Assert.Null(node2.Next);
+		}
+
+		[Fact]
+		public void CanPeekQ()
+		{
+			//arrange
+			Queue testQ = new Queue(new Node(1));
+			Node node2 = new Node(2);
+			Node node3 = new Node(3);
+			Node node4 = new Node(4);
+
+			//act
+			testQ.Enqueue(node2);
+			testQ.Enqueue(node3);
+			testQ.Enqueue(node4);
+
+
+			//assert
+			Assert.Equal(1, testQ.Peek().Value);
+		}
+	
+	}
 }
