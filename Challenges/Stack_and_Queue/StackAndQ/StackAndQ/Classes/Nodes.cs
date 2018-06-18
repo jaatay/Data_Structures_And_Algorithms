9ce01609e5e12a586d90e77c1fd526f0a12196cd
Jesse Atay <jesse.atay@gmail.com>
2018-06-17 21:55:08 -0700

    file structure initialized

---
+++ Challenges/Stack_and_Queue/StackAndQ/StackAndQ/Classes/Nodes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackAndQ
+{
+	public class Node
+	{
+		/// <summary>
+		/// gets and sets value property of a node
+		/// </summary>
+		public int Value { get; set; }
+
+		/// <summary>
+		/// gets and sets the next property of a node
+		/// </summary>
+		public Node Next { get; set; }
+
+		/// <summary>
+		/// sets the valute of a node
+		/// </summary>
+		/// <param name="value">integer to use as node value</param>
+		public Node(int value)
+		{
+			Value = value;
+		}
+	}
+}
