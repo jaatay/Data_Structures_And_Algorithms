24149535a54a0a3b852f23901f8d7efeafdccbec
Jesse Atay <jesse.atay@gmail.com>
2018-06-24 19:09:17 -0700

    binary tree finished, updating files

--- Data_Structures/Trees/Trees/Classes/BinaryTree.cs
+++ Data_Structures/Trees/Trees/Classes/BinaryTree.cs
@@ -1,22 +1,21 @@
 ﻿using System;
-using System.Collections;
-
+using System.Collections.Generic;
+using System.Text;
 
 
 namespace Trees
 {
 
 	public class BinaryTree
-
-
 	{
+
 		public void PreOrder(Node node)
 		{
 			Console.WriteLine(node.Value);
 
 			if(node.LeftChild != null)
 			{
-				PreOrder(node.Leftchild);
+				PreOrder(node.LeftChild);
 			}
 
 			if(node.RightChild != null)
@@ -45,7 +44,7 @@ namespace Trees
 		{
 			if(node.LeftChild != null)
 			{
-				PostOrder(node.Leftchild);
+				PostOrder(node.LeftChild);
 			}
 
 			if(node.RightChild != null)
@@ -58,14 +57,58 @@ namespace Trees
 
 		public void BreadthFirst(Node node)
 		{
-			Queue<Node> breadth = new Queue<Node><Node>();
-			breadth.Enqueue(root);
+			Queue<Node> breadth = new Queue<Node>();
+			breadth.Enqueue(node);
+
+			while(breadth.TryPeek(out node))
+			{
+				Node front = breadth.Dequeue();
+				Console.Write(front.Value);
+
+				if(front.LeftChild != null)
+				{
+					breadth.Enqueue(front.LeftChild);
+				}
+				if(front.RightChild != null)
+				{
+					breadth.Enqueue(front.RightChild);
+				}
+			}
 
 		}
 
-		public void Search(Node root, int value)
+		public bool Search(Node node, int value)
 		{
+			Queue<Node> breadth = new Queue<Node>();
+			breadth.Enqueue(node);
+			Console.WriteLine($"Searching for node value: {value}");
+			try
+			{
+				while (breadth.TryPeek(out node))
+				{
+					Node front = breadth.Dequeue();
 
+					if (front.LeftChild.Value == value || front.RightChild.Value == value)
+					{
+						Console.WriteLine($"Found Node: {value}");
+						return true;
+					}
+					if (front.LeftChild != null)
+					{
+						breadth.Enqueue(front.LeftChild);
+					}
+					if (front.RightChild != null)
+					{
+						breadth.Enqueue(front.RightChild);
+					}
+				}
+				throw new NullReferenceException();
+			} catch(NullReferenceException)
+			{
+				Console.WriteLine("Value not found");
+				return false;
+			}
+		
 		}
 	}
 }
