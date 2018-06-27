69c6cca69151609ffd1226d3dfc6eb03c2b1d650
Jesse Atay <jesse.atay@gmail.com>
2018-06-27 10:36:42 -0700

    tests and readme updates

---
+++ Challenges/FizzBuzzTree/FizzBuzzTree/Classes/Node.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+namespace FizzBuzzTree
+{
+
+	public class Node
+	{
+		/// <summary>
+		/// gets and sets value property of a node
+		/// </summary>
+		public string Value { get; set; }
+
+		/// <summary>
+		/// gets and sets the next property of a node
+		/// </summary>
+		public Node LeftChild { get; set; }
+
+		public Node RightChild { get; set; }
+
+		public Node Root { get; set; }
+
+		/// <summary>
+		/// sets the valute of a node
+		/// </summary>
+		/// <param name="value">integer to use as node value</param>
+		public Node(string value)
+		{
+			Value = value;
+		}
+
+	}
+}
