24e991d2e8a1d3db557e23c275fa57011004dfca
Jesse Atay <jesse.atay@gmail.com>
2018-06-27 18:16:13 -0700

    add test updates

---
+++ Challenges/MaximumValueBinaryTree/MaxTreeTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using MaximumValueBinaryTree;
+
+namespace MaxTreeTest
+{
+    public class TreeTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Challenges/MaximumValueBinaryTree/MaximumValueBinaryTree/Classes/BinaryTree.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace MaximumValueBinaryTree
+{
+
+	public class BinaryTree
+	{
+
+
+		/// <summary>
+		/// method to traverse using Bread-First
+		/// </summary>
+		/// <param name="node">input node to set as root of traversal</param>
+		public int MaxTree(Node node)
+		{
+			Queue<Node> bread = new Queue<Node>();
+			bread.Enqueue(node);
+			int tempInt = 0;
+
+			while (bread.TryPeek(out node))
+			{
+				Node front = bread.Dequeue();
+				
+				if (tempInt < front.Value)
+				{
+					tempInt = front.Value;
+				}
+		
+				//continue node traversal
+				if (front.LeftChild != null)
+				{
+					bread.Enqueue(front.LeftChild);
+				}
+
+				if (front.RightChild != null)
+				{
+					bread.Enqueue(front.RightChild);
+				}
+				//extra line so node old and new values end up side by side
+				
+			}
+			Console.WriteLine(tempInt);
+			return tempInt;
+		}
+	}
+}
