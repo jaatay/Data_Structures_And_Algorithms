2539840328797aec12a136062b352a8411848c07
Jesse Atay <jesse.atay@gmail.com>
2018-07-09 22:47:07 -0700

    updates

---
+++ Data_Structures/Graphs/Graphs/Classes/Graph.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Graphs
+{
+	public class Graph
+	{
+
+
+		public Node Root { get; set; }
+
+		public List<Node> ListNodes { get; set; }
+
+		/// <summary>
+		/// method to add a neighbor node
+		/// </summary>
+		/// <param name="root">root node to add from</param>
+		/// <param name="destination">destination node to add to</param>
+		public void AddNeighbor(Node root, Node destination)
+		{
+			root.Neighbor.Add(destination);
+			destination.Neighbor.Add(root);
+		}
+
+		/// <summary>
+		/// method to get all nodes
+		/// </summary>
+		/// <param name="node">root node to execute request from</param>
+		/// <returns>list of nodes gotten</returns>
+		public List<Node> GetNodes(Node root)
+		{
+			List<Node> order = new List<Node>();
+			Queue<Node> breadth = new Queue<Node>();
+			breadth.Enqueue(root);
+
+			while (breadth.TryPeek(out root))
+			{
+				Node front = breadth.Dequeue();
+				order.Add(front);
+
+				foreach (Node child in front.Neighbor)
+				{
+					if (!child.Visited)
+					{
+						child.Visited = true;
+						breadth.Enqueue(child);
+					}
+				}
+
+			}
+
+			//print list
+			foreach (var item in order)
+			{
+				Console.WriteLine(item.Value);
+			}
+
+			return order;
+		}
+
+		/// <summary>
+		/// method to get neighbors in a graph
+		/// </summary>
+		/// <param name="root">root node to initiate method from</param>
+		/// <returns>list of neighbors</returns>
+		public List<Node> GetNeighbors(Node root)
+		{
+			List<Node> result = new List<Node>();
+
+			for (var i = 0; i < root.Neighbor.Count; i++)
+			{
+				result.Add(root.Neighbor[i]);
+			}
+
+			Console.WriteLine($"Node {root.Value} neighbors = ");
+
+			foreach (var item in result)
+			{
+				Console.WriteLine($"{item.Value}");
+			}
+
+			return result;
+		}
+
+		/// <summary>
+		/// method to get the size of the list
+		/// </summary>
+		/// <param name="nodeList">list of nodes returned from traversal</param>
+		/// <returns>integer of total amount of nodes/vertices</returns>
+		public int Size(List<Node> nodeList)
+		{
+			int totalCount = 0;
+
+			foreach (var item in nodeList)
+			{
+				totalCount++;
+			}
+
+			Console.WriteLine($"The total count is {totalCount}");
+			return totalCount;
+		}
+
+		/// <summary>
+		/// method to traverse bread-first
+		/// </summary>
+		/// <param name="node">root node to execute request from</param>
+		/// <returns>list of nodes gotten</returns>
+		public List<Node> BreadFirst(Node root)
+		{
+			List<Node> order = new List<Node>();
+			Queue<Node> breadth = new Queue<Node>();
+			breadth.Enqueue(root);
+
+			while (breadth.TryPeek(out root))
+			{
+				Node front = breadth.Dequeue();
+				order.Add(front);
+
+				foreach (Node child in front.Neighbor)
+				{
+					if (!child.Visited)
+					{
+						child.Visited = true;
+						breadth.Enqueue(child);
+					}
+				}
+
+			}
+
+			//print list
+			foreach (var item in order)
+			{
+				Console.WriteLine(item.Value);
+			}
+
+			return order;
+		}
+	}
+}
--- Data_Structures/Graphs/Graphs/Program.cs
+++ Data_Structures/Graphs/Graphs/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
 
 namespace Graphs
 {
@@ -7,6 +10,171 @@ namespace Graphs
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+			TestMethods();
+			TestMethodGetNodes();
+			TestMethodBreadFirst();
+			Console.WriteLine("Press any key to exit.");
+			Console.ReadLine();
         }
-    }
+
+		public static void TestMethods()
+		{
+			Graph newGraph = new Graph();
+
+			Node nodeA = new Node("A");
+			
+			Node nodeB = new Node("B");
+
+			Node nodeC = new Node("C");
+
+			Node nodeD = new Node("D");
+
+			Node nodeE = new Node("E");
+
+			Node nodeF = new Node("F");
+
+			//add node A neighbors
+			nodeA.Neighbor.Add(nodeB);
+			nodeA.Neighbor.Add(nodeD);
+
+			//add node B neighbors
+			nodeB.Neighbor.Add(nodeA);
+			nodeB.Neighbor.Add(nodeC);
+			nodeB.Neighbor.Add(nodeE);
+
+			//add node C neighbors
+			nodeC.Neighbor.Add(nodeB);
+			nodeC.Neighbor.Add(nodeF);
+
+			//add node D neighbors
+			nodeD.Neighbor.Add(nodeA);
+			nodeD.Neighbor.Add(nodeE);
+
+			//add node E neighbors
+			nodeE.Neighbor.Add(nodeB);
+			nodeE.Neighbor.Add(nodeD);
+			nodeE.Neighbor.Add(nodeA);
+			nodeE.Neighbor.Add(nodeF);
+
+			//add node F neighbors
+			nodeF.Neighbor.Add(nodeE);
+			nodeF.Neighbor.Add(nodeC);
+
+			//newGraph.ListNodes.Add(nodeB);
+
+			//get neighbors
+			Console.WriteLine("Get Neighbors");
+			newGraph.GetNeighbors(nodeA);
+			Console.WriteLine("------------");
+
+			//get size
+			Console.WriteLine("Get size");
+			var myGraph = newGraph;
+			myGraph.Size(newGraph.GetNodes(nodeA));
+			Console.WriteLine("------------");
+		}
+
+		/// <summary>
+		/// method to initiate new graph and run test methods. Since Bread-first traversal was used in graph methods the original graph nodes get dequeued and a new graph is necessary
+		/// </summary>
+		public static void TestMethodGetNodes()
+		{
+			Graph newGraph = new Graph();
+
+			Node nodeA = new Node("A");
+
+			Node nodeB = new Node("B");
+
+			Node nodeC = new Node("C");
+
+			Node nodeD = new Node("D");
+
+			Node nodeE = new Node("E");
+
+			Node nodeF = new Node("F");
+
+			//add node A neighbors
+			nodeA.Neighbor.Add(nodeB);
+			nodeA.Neighbor.Add(nodeD);
+
+			//add node B neighbors
+			nodeB.Neighbor.Add(nodeA);
+			nodeB.Neighbor.Add(nodeC);
+			nodeB.Neighbor.Add(nodeE);
+
+			//add node C neighbors
+			nodeC.Neighbor.Add(nodeB);
+			nodeC.Neighbor.Add(nodeF);
+
+			//add node D neighbors
+			nodeD.Neighbor.Add(nodeA);
+			nodeD.Neighbor.Add(nodeE);
+
+			//add node E neighbors
+			nodeE.Neighbor.Add(nodeB);
+			nodeE.Neighbor.Add(nodeD);
+			nodeE.Neighbor.Add(nodeA);
+			nodeE.Neighbor.Add(nodeF);
+
+			//add node F neighbors
+			nodeF.Neighbor.Add(nodeE);
+			nodeF.Neighbor.Add(nodeC);
+			//get nodes in graph
+			Console.WriteLine("Get Nodes in graph");
+			newGraph.GetNodes(nodeA);
+			Console.WriteLine("------------");
+		}
+
+		/// <summary>
+		/// method to instantiate new graph
+		/// </summary>
+		public static void TestMethodBreadFirst()
+		{
+			Graph newGraph = new Graph();
+
+			Node nodeA = new Node("A");
+
+			Node nodeB = new Node("B");
+
+			Node nodeC = new Node("C");
+
+			Node nodeD = new Node("D");
+
+			Node nodeE = new Node("E");
+
+			Node nodeF = new Node("F");
+
+			//add node A neighbors
+			nodeA.Neighbor.Add(nodeB);
+			nodeA.Neighbor.Add(nodeD);
+
+			//add node B neighbors
+			nodeB.Neighbor.Add(nodeA);
+			nodeB.Neighbor.Add(nodeC);
+			nodeB.Neighbor.Add(nodeE);
+
+			//add node C neighbors
+			nodeC.Neighbor.Add(nodeB);
+			nodeC.Neighbor.Add(nodeF);
+
+			//add node D neighbors
+			nodeD.Neighbor.Add(nodeA);
+			nodeD.Neighbor.Add(nodeE);
+
+			//add node E neighbors
+			nodeE.Neighbor.Add(nodeB);
+			nodeE.Neighbor.Add(nodeD);
+			nodeE.Neighbor.Add(nodeA);
+			nodeE.Neighbor.Add(nodeF);
+
+			//add node F neighbors
+			nodeF.Neighbor.Add(nodeE);
+			nodeF.Neighbor.Add(nodeC);
+			//bread-first traversal
+			Console.WriteLine("Bread-first traversal");
+			newGraph.BreadFirst(nodeA);
+			Console.WriteLine("------------");
+		}
+	}
 }
